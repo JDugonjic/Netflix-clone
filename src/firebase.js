a08ee05570f85cf62edc9fffa845e1b084740b90
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-02-04 17:21:21 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTIM-uoUSG7z0SK-07JjBxgNTj-bCgABs",
+  authDomain: "nflix-clone.firebaseapp.com",
+  projectId: "nflix-clone",
+  storageBucket: "nflix-clone.appspot.com",
+  messagingSenderId: "1068706824817",
+  appId: "1:1068706824817:web:b73d1de1e0bca64f6351d0",
+};
